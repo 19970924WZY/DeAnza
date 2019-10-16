85089f84542011360d6bcbab7a34bcc1a2349bd3
Zhengyu Wu <36287486+19970924WZY@users.noreply.github.com>
2019-10-16 13:49:03 -0700

    Add files via upload

---
+++ CIS36A/HW1/FutureGoal/src/FutureGoals.java
@@ -0,0 +1,21 @@
+/*
+ * 	@Zhengyu Wu
+ * 	CIS 36A TTh 11:30am - 01:20pm
+ * 	De Anza College
+ * 	Professor: Jennifer Parrish
+ */
+public class FutureGoals {
+	public static void main(String[] args) {
+		System.out.println("Q: Where do you see yourself in 5 years?");
+		System.out.println("A: In five years, I hope I can find a good job.\n");
+		System.out.println("Q: Where do you see yourself in 10 years?");
+		System.out.println("A: In ten years, I will go back to China and living there.\n");
+		System.out.println("Q: Where do you see yourself in 15 years?");
+		System.out.println("A: In fifteen years, I may not marry to anyone.\n");
+		System.out.println("Q: Where do you see yourself in 25 years?");
+		System.out.println("A: In twenty-five years, I will go back to my hometown.\n");
+		System.out.println("Q: Where do you see yourself in 50 years?");
+		System.out.println("A: In fifty years, I will be retired.\n");
+		
+	}
+}
