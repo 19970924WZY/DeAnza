f949f158e4b3775b01cc7fd6e6f0c0480bedb2eb
Zhengyu Wu <36287486+19970924WZY@users.noreply.github.com>
2019-10-16 13:49:34 -0700

    Add files via upload

---
+++ CIS36A/HW1/Banner/Banner.java
@@ -0,0 +1,17 @@
+/*
+ * 	@Zhengyu Wu
+ * 	CIS 36A TTh 11:30am - 01:20pm
+ * 	De Anza College
+ * 	Professor: Jennifer Parrish
+ */
+public class Banner {
+	public static void main(String[] args) {
+		//Banner name : ZHENGYU
+		System.out.println("The name I am gonna use for this assignment is ZHENGYU.");
+		System.out.println("ZZZZ H  H EEEE N  N  GG   Y  Y U  U");
+		System.out.println("  Z  H  H E    NN   G     Y  Y U  U");
+		System.out.println(" Z   HHHH EEEE NNNN G GG   YY  U  U");
+		System.out.println("Z    H  H E    N NN G  G   YY  U  U");
+		System.out.println("ZZZZ H  H EEEE N  N GGGG   YY  UUUU");
+	}
+}
